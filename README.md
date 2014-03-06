57ec94350dbc11ec4e5ae170d22ebb0f0c4bb83c
evamvid <evamvid@gmail.com>
2014-03-06 17:05:57 -0500

    Added GUI version and Updated readme.md

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ A simple python script that converts a .pages document into a PDF. GUI version, 
 
 The Scripts
 ===========
-Pages-zip.py is the working one, authored by me but tweaked extensively by [Stack Overflow user martineau](http://stackoverflow.com/questions/22161088/how-to-extract-a-file-within-a-folder-within-a-zip-in-python).
+pages-pdf is the working console version, authored by me be refactored and overhauled by [StackOverflow user Martineau]()(see below)
 
-Pages-zip old.py is the original, authored only by me, which doesn't work (because I couldn't make it work without martineau's help).
-
-Have fun!
+Attributions/Acknowledgements
+=============================
+If not for the help of StackOverflow users [Martineau](), [](), [J.F. Sebastian](http://stackoverflow.com/users/4279/j-f-sebastian) [](), I probably would not have the program running right now.
+You can see the individual questions where they completely overhauled my code until it was barely recognizable as mine [here](), [here](), and [here]().
---
+++ pages-pdf-gui.py
@@ -0,0 +1,51 @@
+from Tkinter import *
+from tkFileDialog import *
+import os.path
+import shutil
+import sys
+import tempfile
+from zipfile import ZipFile
+import subprocess
+
+
+class uiclass():
+    def __init__(self,root):
+        b = Button(root, text="Browse", command=self.callback)
+        w = Label(root, text="Please choose a .pages file to convert.")
+        w.pack()
+        b.pack()
+
+    def callback(self):
+        global y
+        y = askopenfilename(parent=root, defaultextension=".pages")
+        self.view_file(y)
+		
+    def view_file(self,filepath):
+
+        PREVIEW_PATH = 'QuickLook/Preview.pdf'  # archive member path
+        #pages_file = raw_input('Enter the path to the .pages file in question: ')
+        pages_file = y
+        pages_file = os.path.abspath(pages_file)
+        filename, file_extension = os.path.splitext(pages_file)
+        if file_extension == ".pages":
+            tempdir = tempfile.gettempdir()
+            temp_filename = os.path.join(tempdir, PREVIEW_PATH)
+            with ZipFile(pages_file, 'r') as zipfile:
+                zipfile.extract(PREVIEW_PATH, tempdir)
+            if not os.path.isfile(temp_filename):  # extract failure?
+                sys.exit('unable to extract {} from {}'.format(PREVIEW_PATH, pages_file))
+            final_PDF = filename + '.pdf'
+            shutil.copy2(temp_filename, final_PDF)  # copy and rename extracted file
+            # delete the temporary subdirectory created (along with pdf file in it)
+            shutil.rmtree(os.path.join(tempdir, os.path.split(PREVIEW_PATH)[0]))
+            print('Check out the PDF! It\'s located at "{}".'.format(final_PDF))
+            subprocess.Popen(filename + ".pdf", shell=True).wait()
+            file_extension = ".pdf"
+        else:
+            sys.exit('Sorry, that isn\'t a .pages file.')
+
+if __name__ == '__main__':
+    root = Tk()
+    uiclass(root)
+    root.wm_title("Pages to PDF")
+    root.mainloop()
