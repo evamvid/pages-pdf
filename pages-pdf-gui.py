f372e97e6cbf4bb554a0d11168bbbedf3e1acb5b
evamvid <evamvid@gmail.com>
2014-03-10 20:15:38 -0400

    Restructured dir and updated script
    moved pages-pdf-old to legacy, renamed console version to
    pages-pdf-console, delete Python 2.7 install script, and updated main
    pages-pdf-gui

--- pages-pdf-gui.py
+++ pages-pdf-gui.py
@@ -12,22 +12,32 @@ class uiclass():
         global w
         global quit
         global errlbl
+        global restart
         global b
         global fbv
         global fv
         global qbt
+        global cantOpen
         w = Label(root, text="Please choose a .pages file to convert.")
         quit = Label(root, text="Press 'q' to quit at any time")
         errlbl = Label(root, text="Error converting file.")
+        restart = Button(root, text="Click here to restart", command=self.restart)
         b = Button(root, text="Browse", command=self.callback)
         fbv = Button(root, text="Click here to open PDF location", command=self.dirview)
         fv = Button(root, text="Click here to open PDF", command=self.fileview)
         qbt = Button(root, text="Click here to quit", command=self.qbt)
-        frame = Frame(root, width=100)
+        frame = Frame(root, width=200)
+        cantOpen = Label(root, text="This is not a valid .pages file")
         w.pack()
         b.pack()
         quit.pack()
         frame.pack()
+    
+    def restart(self):
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+        root.destroy()
+    
     def dirview(self):
         if sys.platform=='win32':
             viewfile = filename + ".pdf"
@@ -38,7 +48,7 @@ class uiclass():
             try:
                 subprocess.Popen(['xdg-open', viewfile])
             except OSError:
-                pass
+                cantOpen.pack()
         
     def callback(self):
         global y
@@ -55,37 +65,42 @@ class uiclass():
         root.destroy()
 		
     def view_file(self,filepath):
-
-        PREVIEW_PATH = 'QuickLook/Preview.pdf'  # archive member path
-        #pages_file = raw_input('Enter the path to the .pages file in question: ')
-        global pages_file
-        pages_file = y
-        pages_file = os.path.abspath(pages_file)
-        global filename
-        filename, file_extension = os.path.splitext(pages_file)
-        if file_extension == ".pages":
-            tempdir = tempfile.gettempdir()
-            temp_filename = os.path.join(tempdir, PREVIEW_PATH)
-            with ZipFile(pages_file, 'r') as zipfile:
-                zipfile.extract(PREVIEW_PATH, tempdir)
-            if not os.path.isfile(temp_filename):  # extract failure?
-                errlbl = Label (root, text = "Unable to extract {} from {}.".format(PREVIEW_PATH, pages_file))
-                errlbl.pack()
-            final_PDF = filename + '.pdf'
-            shutil.copy2(temp_filename, final_PDF)  # copy and rename extracted file
-            shutil.rmtree(os.path.join(tempdir, os.path.split(PREVIEW_PATH)[0])) # delete the temporary subdirectory created (along with pdf file in it)
-            fv.pack()
-            fbv.pack()
+        try:
+            PREVIEW_PATH = 'QuickLook/Preview.pdf'  # archive member path
+            #pages_file = raw_input('Enter the path to the .pages file in question: ')
+            global pages_file
+            pages_file = y
+            pages_file = os.path.abspath(pages_file)
+            global filename
+            filename, file_extension = os.path.splitext(pages_file)
+            if file_extension == ".pages":
+                tempdir = tempfile.gettempdir()
+                temp_filename = os.path.join(tempdir, PREVIEW_PATH)
+                with ZipFile(pages_file, 'r') as zipfile:
+                    zipfile.extract(PREVIEW_PATH, tempdir)
+                if not os.path.isfile(temp_filename):  # extract failure?
+                    errlbl = Label (root, text = "Unable to extract {} from {}.".format(PREVIEW_PATH, pages_file))
+                    errlbl.pack()
+                final_PDF = filename + '.pdf'
+                shutil.copy2(temp_filename, final_PDF)  # copy and rename extracted file
+                shutil.rmtree(os.path.join(tempdir, os.path.split(PREVIEW_PATH)[0])) # delete the temporary subdirectory created (along with pdf file in it)
+                fv.pack()
+                fbv.pack()
+                qbt.pack()
+                file_extension = ".pdf"
+            else:
+                global invalid
+                invalid = Label(root, text='Sorry, that isn\'t a .pages file.')
+                invalid.pack()
+                qbt.pack()
+                restart.pack()
+        except:
+            global invalid2
+            invalid2 = Label(root, text='Sorry, that isn\'t a valid .pages file.')
+            invalid2.pack()
             qbt.pack()
-            file_extension = ".pdf"
-        else:
-            global invalid
-            invalid = Label(root, text='Sorry, that isn\'t a .pages file.')
-            invalid.pack()
-            quit = Label(root, text="Press 'q' to quit...")
+            restart.pack()
             
-            
-
 def quitfn(event=None):   
     root.destroy()
             
--- py2.7install.bat
+++
@@ -1,4 +0,0 @@
-cd C:\
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
-cinst python
-REM This Script will install Chocolatey and Python 2.7 (2.7.6 to be exact), which is what my program is written in.
